8d2c1dc69d25e91ab799143f45eefbe33d316831
jason <jsongo@qq.com>
2016-10-21 11:01:10 +0800

    fix bugs

--- pages/my/my.js
+++ pages/my/my.js
@@ -2,12 +2,43 @@
 var app = getApp()
 Page({
     data: {
-        desc: 'my center Page'
+        actionSheetHidden: true,
+        // msg
+        toastHidden: true,
+        toastMsg: 'message'
     },
     onLoad: function () {
-      console.log('onLoad');
-      wx.setNavigationBarTitle({
-          title:"个人中心"
-      });
+        console.log('onLoad');
+        wx.setNavigationBarTitle({
+            title:"个人中心"
+        });
+    },
+    logout: function() {
+        this.setData({
+            actionSheetHidden: false
+        });
+    },
+    doLogout: function() {
+        // TODO: do some logout action here
+        this.actionSheetChange();
+        this.toast('注销成功！');
+    },
+    actionSheetChange: function() {
+        this.setData({
+            actionSheetHidden: !this.data.actionSheetHidden
+        });
+    },
+    toast: function(msg) {
+        this.setData({
+            toastHidden: false,
+            toastMsg: msg
+        });
+    },
+    toastChange: function(event) {
+        if (event.detail.value === false) {
+            this.setData({
+                toastHidden: true
+            });
+        }
     }
 })
